3f5a0333167c10fdaaa218a2e7a3f5aed70ab839
brunooassuncao <168558135+brunooassuncao@users.noreply.github.com>
2024-05-20 18:31:12 -0300

    Update codigo.py
    criei uma função que sugere uma receita pré-pronta de maneira aleatória

--- codigo.py
+++ codigo.py
@@ -4,3 +4,16 @@ def printar_favoritas():
     for i in file:
         i = i.strip()
         visualizar(i)
+def aleatoria():
+    vetor = []
+    file=open("receitas.txt","r", encoding="utf8")
+    for linha in file:
+     vetor.append(linha) 
+
+    file.close()
+    nome = random.choice(vetor)
+    nome = nome.strip()
+    receita = open(f"{nome}.txt","r",encoding="utf8")
+    print(receita.read())
+    receita.close()
+
